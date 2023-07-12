ff0a67ddce851c4a723d84bdf7f123866ac09298
Elijah <elijah@nyp-itsours.com>
2023-07-12 19:36:32 -0400

    made build.sh use buildx

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker buildx create --name kbin-builder
 docker buildx use kbin-builder
 
-docker build -t ghcr.io/elijahnyp/kbin-alternate-docker:1fe3fd3d -t ghcr.io/elijahnyp/kbin-alternate-docker:latest -f Dockerfile .
+docker buildx build -t ghcr.io/elijahnyp/kbin-alternate-docker:1fe3fd3d -t ghcr.io/elijahnyp/kbin-alternate-docker:latest -f Dockerfile .
