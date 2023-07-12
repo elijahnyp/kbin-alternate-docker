c89c65ec57f4256fb903f06796a7671d06f32da1
Elijah <elijah@nyp-itsours.com>
2023-07-12 19:32:47 -0400

    fixed update

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-KBIN_VERSION=$(cd $SCRIPT_DIR/kbin-core; git rev-parse --short HEAD; cd ..)
+KBIN_VERSION=$(cd $SCRIPT_DIR/kbin-core; git rev-parse --short=8 HEAD; cd ..)
 
-sed -i "s/\(ghcr\.io\/elijahnyp\/kbin-alternate-docker:\)[^ ]\{8\}/\1$KBIN_VERSION/" build.sh
-sed -i "s/\(ghcr\.io\/elijahnyp\/kbin-alternate-docker:\)[^ ]\{8\}/\1$KBIN_VERSION/" build_prod.sh
+sed -i "s/\(ghcr\.io\/elijahnyp\/kbin-alternate-docker:\)[^ ]\{8,20\}/\1$KBIN_VERSION/" build.sh
+sed -i "s/\(ghcr\.io\/elijahnyp\/kbin-alternate-docker:\)[^ ]\{8,20\}/\1$KBIN_VERSION/" build_prod.sh
 sed -i "s/^\(appVersion: \).*$/\1\"$KBIN_VERSION\"/" Chart/kbin/Chart.yaml
