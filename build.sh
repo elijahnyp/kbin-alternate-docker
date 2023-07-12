810d1634687cc090466e1a5b03f412fc8a4d0b89
Elijah <elijah@nyp-itsours.com>
2023-07-12 18:27:12 -0400

    version bump of kbin, converted to using commit short hash as container version

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker buildx create --name kbin-builder-dev
 docker buildx use kbin-builder-dev
 
-docker build -t ghcr.io/elijahnyp/kbin-alternate-docker:0.0.3 -f Dockerfile .
+docker build -t ghcr.io/elijahnyp/kbin-alternate-docker:1fe3fd3dd4 -t ghcr.io/elijahnyp/kbin-alternate-docker:latest-f Dockerfile .
